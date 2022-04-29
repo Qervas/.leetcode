4074c9e6fde87e7c77443b85e946d80b04c1238d
Qervas <djmax96945147@outlook.com>
2022-04-30 01:58:37 +0800

    update

--- 912排序数组/912.排序数组.cpp
+++ 912排序数组/912.排序数组.cpp
@@ -3,7 +3,7 @@
  * @Date: 2022-04-27 00:56:25
  * @LastEditors: FrankTudor
  * @Description: This file is created, edited, contributed by FrankTudor
- * @LastEditTime: 2022-04-30 00:40:25
+ * @LastEditTime: 2022-04-30 01:55:41
  */
 /*
  * @lc app=leetcode.cn id=912 lang=cpp
@@ -38,20 +38,20 @@ public:
 
 
     //####Sort####
-    // vector<int> sortArray(vector<int>& nums) {
+    // vector<int> sortnumsay(vector<int>& nums) {
 
     // }
     //####Sort####
-    // vector<int> sortArray(vector<int>& nums) {
+    // vector<int> sortnumsay(vector<int>& nums) {
 
     // }
     //####Sort####
-    // vector<int> sortArray(vector<int>& nums) {
+    // vector<int> sortnumsay(vector<int>& nums) {
 
     // }
 
     //####Sort####
-    // vector<int> sortArray(vector<int>& nums) {
+    // vector<int> sortnumsay(vector<int>& nums) {
 
     // }
 
@@ -104,7 +104,7 @@ public:
     //         nums[iter_nums] = sorted[iter_sorted];
     //     }
     // }
-    // vector<int> sortArray(vector<int>& nums) {
+    // vector<int> sortnumsay(vector<int>& nums) {
     //     vector<int> sorted(nums.size());
     //     insertionMergeSort(0,nums.size(), nums, sorted);
     //     // inPlaceMergeSort(0, nums.size(), nums);
@@ -117,7 +117,7 @@ public:
     //          (Obviously, it doesn't make much sense to sort a sequence sorted by 4 and then sort by 2).
     //Your runtime beats 52.38 % of cpp submissions
     //Your memory usage beats 76.72 % of cpp submissions (27.7 MB)
-    // vector<int> sortArray(vector<int>& nums) {
+    // vector<int> sortnumsay(vector<int>& nums) {
     //     int h = 1;
     //     while(h < nums.size() /3){
     //         h = 3*h+1;
@@ -136,23 +136,23 @@ public:
     //####Insertion Sort####
     // Time Limit Exceeded
     // 10/13 cases passed (N/A)
-    // vector<int> sortArray(vector<int>& nums) {
-    //     for(int i = 1; i < nums.size(); i++){
-    //         int j=i;
-    //         while(j > 0){
-    //             if(nums[j] < nums[j-1]){
-    //                 swap(nums[j-1],nums[j]);
-    //                  j--;
-    //             }else break;
-    //         }
-    //     }
-    //     return nums;
-    // }
+    vector<int> insertionSort(vector<int>& nums) {
+        for(int i = 1; i < nums.size(); i++){
+            int j=i;
+            while(j > 0){
+                if(nums[j] < nums[j-1]){
+                    swap(nums[j-1],nums[j]);
+                     j--;
+                }else break;
+            }
+        }
+        return nums;
+    }
 
     //####Selection Sort####
     // Time Limit Exceeded
     // 10/13 cases passed (N/A)
-    // vector<int> sortArray(vector<int>& nums) {
+    // vector<int> sortnumsay(vector<int>& nums) {
     //     int min;
     //     int length = nums.size();
     //     for(int i = 0,j; i < length-1; i++){
@@ -167,7 +167,7 @@ public:
     //####Bubble Sort####
     // Time Limit Exceeded
     // 10/13 cases passed (N/A)
-    // vector<int> sortArray(vector<int>& nums) {
+    // vector<int> sortnumsay(vector<int>& nums) {
     //     int end = nums.size() -1;
     //     for(int i = 0, j;i <= end; i++){
     //         for(j = end; j > i; j--){
@@ -181,45 +181,22 @@ public:
 
     //####Quick Sort####
     int partition(int lo, int hi, vector<int>& nums){//[lo,hi)
-        if(lo >= hi - 1){return lo;}
-        int base = nums[lo];//randomization is better
-        int i = lo + 1;
-        int j = hi - 1;
-        while(i < j){
-            while( i < j && nums[i] < base){i++;}
-            while( i < j && nums[j] > base){j--;}
-            //nums[i] >= base && nums[j] <= base;
-            if(nums[i] >= base && base >= nums[j]){//nums[i] > x > nums[j]
-                ysx::swap(nums[i], nums[j]);
-            }
-        }
-        ysx::swap(nums[lo],nums[i]);
-        return i;
         
-        // int x = nums[lo];
-        // int i = lo + 1, j = hi-1;
-        // while(i < j){
-        //     if(nums[i] < x){
-        //         i++;
-        //         continue;
-        //     }
-        //     if(x < nums[j]){
-        //         j--;
-        //         continue;
-        //     }
-        //     if(nums[i] > x && x >= nums[j]){//nums[i] > x > nums[j]
-        //         ysx::swap(nums[i], nums[j]);
-        //         continue;
-        //     }
+        if(lo >= hi - 1){return lo;}
+        ysx::swap(nums[lo], nums[lo+rand()%(hi-lo+1)]);
+        int base = nums[lo];//randomization is better
+        if(hi > nums.size()){hi--;}
+        while(lo < hi){//assert : lo == hi
+            while( lo < hi && nums[hi] >= base){hi--;}
+            nums[lo] = nums[hi];
+            while( lo < hi && nums[lo] < base){lo++;}
+            nums[hi] = nums[lo];
+        }
+        nums[lo] = base;
+        return lo;
 
-        // }
-        // // if(i == hi - 1 || nums[lo] > nums[i]){
-        //     ysx::swap(nums[lo], nums[i]);
-        // // }
-        // // if(j == lo + 1 && nums[lo] > nums[j]){
-        // //     ysx::swap(nums[lo], nums[i]);
-        // // }
-        // return i;
+
+       
     }
     void print(vector<int>& vec){
         for(int& x: vec){
@@ -227,9 +204,15 @@ public:
         }
     }
     vector<int> sortArray(vector<int>& nums) {
-        print(nums);printf("\n");
-        if(nums.empty()){return {};}
-        if(nums.size() == 1){return nums;}
+        // if(nums.empty()){return {};}
+        // if(nums.size() == 1){return nums;}
+        // if(nums.size() == 2){
+        //     if(nums[0] > nums[1]){
+        //         ysx::swap(nums[0], nums[1]);
+        //     }
+        //     return nums;
+        // }
+        if(nums.size() < 7){insertionSort(nums);return nums;}
         int stack[nums.size()];
         int top = -1, hi, lo;
         stack[++top] = 0;
@@ -238,15 +221,12 @@ public:
         while(top >= 0){
             hi = stack[top--];
             lo = stack[top--];
-            if(nums[pivot]== -1){
-                printf("");
-            }
             pivot = partition(lo, hi, nums);
-            print(nums);
-            printf("pivot: %d|| ",nums[pivot]);
-            printf("[%d, %d)",lo,hi);
-            printf("\n");
-            if(pivot - 1 >= lo){//[lo, pivot)
+            // print(nums);
+            // printf("pivot: %d|| ",nums[pivot]);
+            // printf("[%d, %d)",lo,hi);
+            // printf("\n");
+            if(pivot > lo){//[lo, pivot)
                 stack[++top] = lo;
                 stack[++top] = pivot;
                 // printf("lo: [%d, %d)\n",lo, pivot);
@@ -268,6 +248,7 @@ int main(){
     Solution s;
     vector<int> vec{-4,0,7,4,9,-5,-1,0,-7,-1};
     s.sortArray(vec);
+    s.print(vec);
 
     return 0;
 }
