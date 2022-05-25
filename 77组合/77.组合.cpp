8801293a9fe6d085451e746423fcbc8a3a2954ba
Qervas <djmax96945147@outlook.com>
2022-05-26 01:49:45 +0800

    May25

--- 77组合/77.组合.cpp
+++ 77组合/77.组合.cpp
@@ -1,26 +1,61 @@
-/*
- * @Author: FrankTudor
- * @Date: 2022-05-25 22:48:02
- * @LastEditors: FrankTudor
- * @Description: This file is created, edited, contributed by FrankTudor
- * @LastEditTime: 2022-05-25 22:48:21
- */
+
 /*
  * @lc app=leetcode.cn id=77 lang=cpp
  *
  * [77] 组合
  */
 #include<vector>
+#include<stack>
 #include<iostream>
 using std::vector;
+using std::stack;
 // @lc code=start
 class Solution {
 public:
-    vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>> ans;
+    vector<int> temp;
+    vector<vector<int>> ans;
 
+    vector<vector<int>> combine(int n, int k) {
+        // 初始化
+        // 将 temp 中 [0, k - 1] 每个位置 i 设置为 i + 1，即 [0, k - 1] 存 [1, k]
+        // 末尾加一位 n + 1 作为哨兵
+        for (int i = 1; i <= k; ++i) {
+            temp.push_back(i);
+        }
+        temp.push_back(n + 1);
+        
+        int j = 0;
+        while (j < k) {
+            ans.emplace_back(temp.begin(), temp.begin() + k);
+            j = 0;
+            // 寻找第一个 temp[j] + 1 != temp[j + 1] 的位置 t
+            // 我们需要把 [0, t - 1] 区间内的每个位置重置成 [1, t]
+            while (j < k && temp[j] + 1 == temp[j + 1]) {
+                temp[j] = j + 1;
+                ++j;
+            }
+            // j 是第一个 temp[j] + 1 != temp[j + 1] 的位置
+            ++temp[j];
+        }
         return ans;
     }
 };
-// @lc code=end
 
+
+//https://leetcode.cn/problems/combinations/solution/zu-he-by-leetcode-solution/
+
+// @lc code=endq
+
+int main(void){
+    Solution s;
+    vector<vector<int> > ans = s.combine(4,2);
+    for(size_t i = 0; i < ans.size(); i++){
+        printf("[");
+        for(size_t j = 0; j < ans.at(i).size(); j++){
+            printf("%d, ",ans.at(i).at(j));
+        }
+        printf("]\n");
+    }
+    printf("End\n");
+    return 0;
+}
